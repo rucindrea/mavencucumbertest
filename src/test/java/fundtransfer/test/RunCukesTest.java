fbde2c7a79fc263f4802cc4e2064efd2438ae15f
Ru Cindrea <ru.cindrea@altom.ro>
2015-01-01 16:06:23 +0200

    latest changes

--- src/test/java/fundtransfer/test/RunCukesTest.java
+++ src/test/java/fundtransfer/test/RunCukesTest.java
@@ -5,6 +5,6 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@Cucumber.Options(format = {"pretty", "html:target/cucumber-html-reports"})
+@Cucumber.Options(format = {"pretty", "html:target/cucumber-html-reports", "json:target/cucumber-html-reports"})
 public class RunCukesTest {
 }
