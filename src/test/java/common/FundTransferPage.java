f4111ec2565fb762af89b84113b6fb4afc24ece0
Ru Cindrea <ru.cindrea@altom.ro>
2015-01-02 11:00:03 +0200

    Added page object model classes

--- src/test/java/common/FundTransferPage.java
+++ src/test/java/common/FundTransferPage.java
@@ -1,7 +1,73 @@
 package common;
 
+import fundtransfer.test.FundTransferStepDefs;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.Augmenter;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by rucindrea on 1/1/15.
  */
+
 public class FundTransferPage {
+
+    private final WebDriver driver;
+
+    public FundTransferPage(WebDriver wd) {
+        //instantiates the webdriver passed from test class
+        this.driver = wd;
+    }
+
+    //Fund Transfer Page Web Elements
+    public String pageUrl = "http://dl.dropbox.com/u/55228056/fundTransfer.html";
+    public By payeeField = By.id("payee");
+    public By amountField = By.id("amount");
+    public By transferButton = By.id("transfer");
+    public By balanceField = By.name("balance");
+    public By messageField = By.id("message");
+    public By titleField = By.xpath("//header/h2");
+    public By subTitleField = By.xpath("//header/div");
+
+    //Constants
+    public final String title = "Transfer Funds Online";
+    public final String subTitle = "Enter a Payee and amount and transfer funds instantly!!";
+
+    //Fund Transfer Page methods/actions
+    public void goToFundsTransferPage() {
+        driver.get(pageUrl);
+        assertEquals(title, driver.findElement(titleField).getText());
+        assertEquals(subTitle, driver.findElement(subTitleField).getText());
+
+    }
+    public void setupInitialBalance(int initialBalance) {
+        if (initialBalance != 2000) {
+            int toTransfer = 0 - (initialBalance - 2000);
+            transferAmountTo(toTransfer, "Jim");
+        }
+    }
+    public void transferAmountTo(int amount, String payeeName) {
+        driver.findElement(payeeField).clear();
+        driver.findElement(payeeField).sendKeys(payeeName);
+        driver.findElement(amountField).clear();
+        driver.findElement(amountField).sendKeys(String.valueOf(amount));
+        driver.findElement(transferButton).click();
+    }
+    public void checkRemainingBalance(int expectedBalance) {
+        if (String.valueOf(expectedBalance).contains(".")) {
+            assertEquals(expectedBalance, driver.findElement(balanceField).getAttribute("value"));
+        } else {
+            String expectedBalanceWithDigits = String.valueOf(expectedBalance) + ".00";
+            assertEquals(expectedBalanceWithDigits, driver.findElement(balanceField).getAttribute("value"));
+        }
+    }
+    public void checkTransferMessage(String expectedMessage) {
+        String message = driver.findElement(messageField).getText();
+        assertEquals(message, expectedMessage);
+    }
 }
+
+
+
--- src/test/java/fundtransfer/test/FundTransferStepDefs.java
+++ src/test/java/fundtransfer/test/FundTransferStepDefs.java
@@ -1,5 +1,6 @@
 package fundtransfer.test;
 
+import common.FundTransferPage;
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
@@ -18,54 +19,42 @@ import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import java.io.File;
 import java.io.IOException;
 
+
 import static org.junit.Assert.assertEquals;
 
 public class FundTransferStepDefs {
-
 	protected WebDriver driver;
-    private Scenario scenario;
-	private int counter = 0;
-	
-	
+    public FundTransferPage fundsTransfer;
+
+	@Given("^I am on the Transfer Funds Online Page$")
+	public void I_am_on_the_Transfer_Funds_Online_Page() {
+		fundsTransfer.goToFundsTransferPage();
+	}
+	@Given("^I have (\\d+) in my account$")
+	public void I_have_in_my_account(int initialBalance) {
+		fundsTransfer.setupInitialBalance(initialBalance);
+	}
+	@When("^I transfer (\\d+) to \"([^\"]*)\"$")
+	public void I_transfer_to(int amount, String payeeName) {
+		fundsTransfer.transferAmountTo(amount, payeeName);
+	}
+	@Then("^I should see \"([^\"]*)\"$")
+	public void I_should_see(String message) {
+		fundsTransfer.checkTransferMessage(message);
+	}
+	@Then("^I should have (\\d+) left in my account$")
+	public void I_should_have_left_in_my_account(int balance) {
+		fundsTransfer.checkRemainingBalance(balance);
+	}
 	@Before
 	public void setUp(Scenario scenario) {
 		//driver = new FirefoxDriver();
 		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/chromedriver");
 		driver = new ChromeDriver();
-        this.scenario = scenario;
-		
+		this.scenario = scenario;
+		fundsTransfer = new FundTransferPage(driver);
+
 	}
-
-
-
-
-	@Given("^I am on the Transfer Funds Online Page$")
-	public void I_am_on_the_Transfer_Funds_Online_Page() throws Throwable {
-		driver.get("http://dl.dropbox.com/u/55228056/fundTransfer.html");
-	}
-
-	@Given("^I have (\\d+) in my account$")
-	public void I_have_in_my_account(int initialBalance) throws Throwable {
-		setupInitialBalance(initialBalance);
-	}
-
-	@When("^I transfer (\\d+) to \"([^\"]*)\"$")
-	public void I_transfer_to(int amount, String payeeName) throws Throwable {
-		transferAmountTo(amount, payeeName);
-	}
-
-
-	@Then("^I should see \"([^\"]*)\"$")
-	public void I_should_see(String message) throws Throwable {
-		checkTransferMessage(message);
-	}
-
-
-	@Then("^I should have (\\d+) left in my account$")
-	public void I_should_have_left_in_my_account(int balance) throws Throwable {
-		checkRemainingBalance(balance);
-	}
-
 	@After
 	public void tearDown(Scenario scenario) {
         if(scenario.isFailed()) {
@@ -75,44 +64,6 @@ public class FundTransferStepDefs {
         }
 		driver.close();
 	}
-
-
-	//helper functions
-
-	private void setupInitialBalance(int initialBalance) throws Throwable {
-		if (initialBalance != 2000) {
-			int toTransder = 0 - (initialBalance - 2000);
-			I_transfer_to(toTransder, "Jim");
-		}
-	}
-
-
-	private void transferAmountTo(int amount, String payeeName) {
-		WebElement payeeField = driver.findElement(By.id("payee"));
-		WebElement amountField = driver.findElement(By.id("amount"));
-		payeeField.clear();
-		payeeField.sendKeys(payeeName);
-		amountField.clear();
-		amountField.sendKeys(String.valueOf(amount));
-		driver.findElement(By.id("transfer")).click();
-	}
-
-	private void checkRemainingBalance(int balance) {
-		WebElement actualBalanceElement = driver.findElement(By.name("balance"));
-		String actualBalance = actualBalanceElement.getAttribute("value");
-		if (String.valueOf(balance).contains(".")) {
-			assertEquals(balance, actualBalance);
-		}
-		else {
-			String expectedBalance = String.valueOf(balance) + ".00";
-			assertEquals(expectedBalance, actualBalance);
-		}
-	}
-
-
-	private void checkTransferMessage(String message) {
-		WebElement actualMessage = driver.findElement(By.id("message"));
-		assertEquals(actualMessage.getText(),message);
-	}
-
 }
+
+
